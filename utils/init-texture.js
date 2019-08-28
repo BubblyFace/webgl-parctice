8a083c6e112c00e59700159215ce24c2e36629bb
BubblyFace <751835145@qq.com>
2019-08-28 11:00:56 +0800

    chore:修复内容

--- utils/init-texture.js
+++ utils/init-texture.js
@@ -4,16 +4,11 @@
  * @desc: init texture 
  */
 
-function initTexture(gl, n, imageUrl) {
+function initTexture(gl, n, image) {
   let texture = gl.createTexture();
   let u_Sample = gl.getUniformLocation(gl.program, 'u_Sampler');
-  let image = new Image();
 
-  image.src = imageUrl;
-
-  image.onload = () => {
-      loadTexture(gl, n, texture, u_Sample, image);
-  }
+  loadTexture(gl, n, texture, u_Sample, image);
 }
 
 
@@ -31,6 +26,10 @@ function loadTexture(gl, n, texture, u_Sample, image) {
 
   // 配置纹理参数
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 
   // 配置纹理图像
   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
